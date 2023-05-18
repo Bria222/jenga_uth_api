e27e641c515b1bea8ef663a63dcdd19193d008a4
Bria222 <briantechologies.co.ke@gmail.com>
2023-05-18 15:48:18 +0300

    add associations to product image

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
+      # Users
       get '/users', to: 'users#index'
       get '/users/:id', to: 'users#show'
-      post '/user/add', to: 'users#create'
-      delete '/user/delete/:id', to: 'users#destroy'
-      patch '/user/update/:id', to: 'users#update'
-      post '/user/login', to: 'authentication#login'
+      post '/users/add', to: 'users#create'
+      delete '/users/delete/:id', to: 'users#destroy'
+      patch '/users/update/:id', to: 'users#update'
+      post '/users/login', to: 'authentication#login'
 
+      # Products
       get '/products', to: 'products#index' do
-      get :image, on: :member 
-    end
-      get '/products/:id', to: 'products#show'do
-      get :image, on: :member 
-    end
-      post '/product/add', to: 'products#create'
-      delete '/product/delete/:id', to: 'products#destroy'
-      patch '/product/update/:id', to: 'products#update'
-      
+        get :image, on: :member 
+      end
+      get '/products/:id', to: 'products#show' do
+        get :image, on: :member 
+      end
+      post '/products/add', to: 'products#create'
+      delete '/products/delete/:id', to: 'products#destroy'
+      patch '/products/update/:id', to: 'products#update'
+      get '/products/categories', to: 'products#categories' # New route for categories
     end
   end
 end
