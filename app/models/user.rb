a81254964d5720de56ccf935900afd0f6999f32c
Bria222 <briantechologies.co.ke@gmail.com>
2023-05-22 14:57:49 +0300

    validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, uniqueness: true, presence: true
     validates :password,  presence: true
-    validates :phone_number, presence: true, numericality: { only_integer: true }, length: { is: 13 }
+    validates :phone_number, presence: true
     def admin?
      role == "admin"
     end
