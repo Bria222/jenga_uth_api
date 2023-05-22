65d79b181d6bcec8ad5c56acd84966c2fa46c81d
Bria222 <briantechologies.co.ke@gmail.com>
2023-05-22 14:28:38 +0300

    add phone number field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_191342) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_112326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_191342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
+    t.json "images"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
@@ -69,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_191342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.bigint "phone_number"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
